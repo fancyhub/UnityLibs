3a0261521c600e63bcca67ce27e4004565d3d051
cunyu.fan <cunyu.fan@garena.com>
2025-02-08 10:50:47 +0800

    Update so

--- UnityLibs/Packages/com.github.fancyhub.unitylibs.streamingassetsfilesystem/CppProj/build_plugin.bat
+++ UnityLibs/Packages/com.github.fancyhub.unitylibs.streamingassetsfilesystem/CppProj/build_plugin.bat
@@ -1,16 +1,16 @@
 ::@echo off
 cd /d %~dp0
 
-::SET NDKROOT=C:\Program Files\Unity\2022.3.5f1\Editor\Data\PlaybackEngines\AndroidPlayer\NDK
-SET NDKROOT=C:\PROGRA~1\Unity\2022.3.5f1\Editor\Data\PlaybackEngines\AndroidPlayer\NDK
+::SET NDKROOT=C:\PROGRA~1\Unity\2022.3.5f1\Editor\Data\PlaybackEngines\AndroidPlayer\NDK
+SET NDKROOT=C:\tools\android_sdk\ndk\23.1.7779620
 
 SET PATH=%PATH%;%NDKROOT%
 
 call ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=Application.mk NDK_APP_DST_DIR=../Runtime/Plugins/Android/$(TARGET_ARCH_ABI)
 
 ::timeout /t 2
-rm -r obj
-rm obj.meta
+rmdir /s obj
+del obj.meta
 :: rm -r ../Runtime/Plugins/Android/*/libc++_shared.so
 ::xcopy .\libs\*.so ..\Runtime\Plugins\Android  /Y /S
 pause
